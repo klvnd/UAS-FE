c08f818d98090d20e87d4e01338d4f663fbe8b5c
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2023-12-08 19:10:40 +0700

    Update login.php

--- be/login.php
+++ be/login.php
@@ -16,7 +16,7 @@
                 <img src="./img/logo.png" alt="">
                 <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-4xl ">Mobil Listrik, Mobil Masa Depan</h1>
                 <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Membangkitkan Masa Depan, Menggerakkan Keberlanjutan: Temukan Revolusi Listrik bersama MobiListrik!</p>
-                <a href="./index.php" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Back to Home
+                <a href="../fe/index.php" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Back to Home
                     <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
