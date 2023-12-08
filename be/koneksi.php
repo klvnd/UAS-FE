99bd466429c3c8a7da792acfe0a1d316053bcd59
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2023-12-08 12:48:22 +0700

    update

--- be/koneksi.php
+++ be/koneksi.php
@@ -1,10 +1,10 @@
 <?php
-$host = "localhost"; 
-$user = "root"; 
-$password = ""; 
-$database = "db_mobil_listrik"; 
+$host = "localhost"; // Ganti dengan nama host database Anda
+$user = "root"; // Ganti dengan nama pengguna database Anda
+$password = ""; // Ganti dengan kata sandi database Anda
+$database = "mobil_listrik"; // Ganti dengan nama database Anda
 
-$koneksi = new mysqli("localhost", "root", "", "db_mobil_listrik");
+$koneksi = new mysqli("localhost", "root", "", "mobil_listrik");
 
 if ($koneksi->connect_error) {
     die("Koneksi Gagal: " . $koneksi->connect_error);
