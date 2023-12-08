4ecb467c385f8480d78730ae726863741c9e00ed
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2023-12-08 16:05:39 +0700

    Update admin.php

--- be/admin.php
+++ be/admin.php
@@ -79,7 +79,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <svg class="h-3.5 w-3.5 mr-2 -ml-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                 </svg>  
-                Add New
+                Tambah data
             </button>
         </div>
 
